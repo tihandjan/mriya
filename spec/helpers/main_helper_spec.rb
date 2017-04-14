3534688fd5e36a187bf2c964e629c99ea1c77976
tihandjan <tihandjan198921@gmail.com>
2017-04-14 15:35:24 +0300

    test helper

--- spec/helpers/main_helper_spec.rb
+++ spec/helpers/main_helper_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe MainHelper, type: :helper do
-  
+    describe 'set_teams method' do
+        it 'returns teams' do
+            teams = FactoryGirl.create(:team)
+            
+            expect(set_teams).to match_array(teams)
+        end
+    end
 end
