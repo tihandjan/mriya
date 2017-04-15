e989891531e9cbd955024a56580d0b8d04e0893f
tihandjan <tihandjan198921@gmail.com>
2017-04-16 00:33:19 +0300

    setup cancancan

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def admin?
+    self.has_role?(:admin)
+  end
 end
