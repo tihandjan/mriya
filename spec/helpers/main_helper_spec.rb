e2019de7d002b051b1fcbbe8455742a73bacc2e5
tihandjan <tihandjan198921@gmail.com>
2017-04-15 22:23:36 +0300

    add russiaqn locale

--- spec/helpers/main_helper_spec.rb
+++ spec/helpers/main_helper_spec.rb
@@ -8,4 +8,12 @@ RSpec.describe MainHelper, type: :helper do
             expect(set_teams).to match_array(teams)
         end
     end
+
+    describe 'set_leagues method' do
+        it 'returns leagues' do
+            leagues = FactoryGirl.create(:league)
+
+            expect(set_leagues).to match_array(leagues)
+        end
+    end
 end
--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -17,7 +17,4 @@
 require 'rails_helper'
 
 RSpec.describe Table, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of(:points) }
-  end
 end
