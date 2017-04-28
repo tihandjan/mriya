e04b269d875a6cf338f0a9f508168df55a778565
tihandjan <tihandjan198921@gmail.com>
2017-04-28 04:23:32 +0300

    add comments

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,10 +1,13 @@
 $(document).on('turbolinks:load', function(){
+
+    // ******** search *******
     $('#search-icon').on('click', function(e){
         e.preventDefault(); 
         $('#search-wrapper').toggle('fast');
     });
-
     $(window).click(function() { 
         $('#search-results').html(''); 
     }); 
+    // ******** search *******   
+     
 })
