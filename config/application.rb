2cf621064decbe0fb2d8e06210cfdbf514ff4a5a
tihandjan <tihandjan198921@gmail.com>
2017-05-23 02:56:10 +0300

    fix ckeditor prod problem

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,10 @@ module Mriya
 
     config.time_zone = 'Moscow'
     config.active_record.default_timezone = :local
+
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     
   end
 end
