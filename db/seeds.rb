deebe7f26612406f680a88007befa76d85d40266
tihandjan <tihandjan198921@gmail.com>
2017-05-23 02:42:20 +0300

    add social net

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 11.times do |i|
     League.create(name: "U-#{i + 7}") unless League.count >= 10
 end
-User.create(email: 'mriya@email.com', password: 'mriyabestdream', password_confirmation: 'mriyabestdream').add_role(:admin) if User.find_by_email('mriya@email.com').blank?
+User.create!(email: 'mriya@email.com', password: 'mriyabestdream', password_confirmation: 'mriyabestdream').add_role(:admin) if User.find_by_email('mriya@email.com').blank?
