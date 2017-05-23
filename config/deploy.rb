46a9eec0ead2e5056e2fedac3870afc419c8bc92
tihandjan <tihandjan198921@gmail.com>
2017-05-23 04:02:14 +0300

    fix bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :rails_env, :production
+set :bundle_binstubs, nil
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
@@ -32,7 +33,7 @@ set :rails_env, :production
 
 ## Linked Files & Directories (Default None):
 set :linked_files, %w{config/database.yml config/secrets.yml}
-set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
