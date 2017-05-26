7a1aad3b30329adf9b60837ebde53a0f27ca6290
tihandjan <tihandjan198921@gmail.com>
2017-05-26 23:01:32 +0300

    fix localse

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -42,4 +42,24 @@ $(document).on('turbolinks:load', function() {
         nextResultsBtn.addClass('active');
     })
     // ******* fixtures end ******** 
+
+    // scroll top start 
+	var offset = 300,
+		offset_opacity = 1200,
+		scroll_top_duration = 700,
+		$back_to_top = $('.cd-top');
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+    // scroll top end
 });
