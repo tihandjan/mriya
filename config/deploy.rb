42cf925bf8082f52b6c12a87614eefe33d0bd7e1
tihandjan <tihandjan198921@gmail.com>
2017-06-02 05:48:20 +0300

    deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,8 +22,6 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-set :rails_env, :production
-set :console_env, :production
 ## Defaults:
 # set :scm,           :git
 # set :branch,        :master
