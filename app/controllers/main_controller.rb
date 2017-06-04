36993e0826d50eae6a38e92c7a9355da37448207
tihandjan <tihandjan198921@gmail.com>
2017-06-04 04:06:59 +0300

    change soe logic

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
     @articles = Article.order(created_at: :desc)
     @video = Video.last
-    @players = Player.where('extract(month from birthday) = ? and extract(day from birthday) = ?', Time.current.month, Time.current.day)
+    @players = Player.where('extract(month from birthday) = ?', Time.current.month)
   end
 
   def search
