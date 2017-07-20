c010036a1ac6e91ea348c6fa74b0cd409b4c826e
tihandjan <tihandjan198921@gmail.com>
2017-07-20 16:56:22 +0300

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,5 +1,5 @@
 # paths
-app_path = "/home/deployer/fcmriya"
+app_path = "/home/deployer/apps/fcmriya"
 working_directory "#{app_path}/current"
 pid               "#{app_path}/current/tmp/pids/unicorn.pid"
 
